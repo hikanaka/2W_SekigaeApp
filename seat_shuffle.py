af73bba3416672ed687b31e43a72776ae17d72ef
hikanaka <roundupusa02@gmail.com>
2018-11-29 13:35:52 +0900

    with文を使ってみた。

--- seat_shuffle.py
+++ seat_shuffle.py
@@ -1,9 +1,27 @@
 import random
 
+# with文でのファイルオープン
+# https://bootcamp-text.readthedocs.io/textbook/5_module.html
 
-members = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-for member in members:
-    print(member)
+
+with open("members.txt", mode="r") as f:
+    members = f.read()
+
+
+print(members)
+
+
+
+
+# def main():
+#
+# if __name__ == '__main__':
+
+
+
+# members = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+# for member in members:
+#     print(member)
 
 
 
